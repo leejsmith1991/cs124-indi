6a74b5db38c2971e1657556e79b574b7a073cfae
Lee Smith <ljs5@aber.ac.uk>
2012-03-23 21:00:15 +0000

    Updates

---
+++ cs124-indi/src/uk/ac/aber/dcs/cs124indi/gui/mainFrame.java
@@ -0,0 +1,15 @@
+package uk.ac.aber.dcs.cs124indi.gui;
+
+import javax.swing.JFrame;
+
+public class mainFrame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4978171281055317618L;
+	
+	public mainFrame(){
+		
+	}
+}
