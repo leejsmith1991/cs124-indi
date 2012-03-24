05ef9d086833f241e89ae313faac21541559f7ed
Lee Smith <ljs5@aber.ac.uk>
2012-03-24 15:33:30 +0000

    updated selection

--- cs124-indi/src/uk/ac/aber/dcs/cs12420/aberpizza/gui/Selection.java
+++ cs124-indi/src/uk/ac/aber/dcs/cs12420/aberpizza/gui/Selection.java
@@ -1,13 +1,21 @@
 package uk.ac.aber.dcs.cs12420.aberpizza.gui;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.SpringLayout;
 
 public class Selection extends JPanel {
-
-	/**
-	 * 
-	 */
+	
+	private Manager manager;
+	
+	private JButton pizzas, sides, drinks;
+	private SpringLayout layout;
+	
 	private static final long serialVersionUID = 5892174426336388551L;
 
-	
+	public Selection(Manager manager) {
+		this.manager = manager;
+		
+		
+	}
 }
